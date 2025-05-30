a789926193777598a960ffca00169bc3f466d996
shadowfeind <travestroy007@gmail.com>
2025-05-30 11:13:25 +0545

    footer

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <footer className={"flex border-t justify-between font-medium p-6"}>
+            <div className={"flex items-center p-2"}>
+                <p>funroad, Inc.</p>
+            </div>
+        </footer>
+    )
+}
--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,10 +1,14 @@
 import {Navbar} from "@/app/(home)/navbar";
+import {Footer} from "@/app/(home)/footer";
 
 const homeLayout = ({children} : {children: React.ReactNode}) => {
     return(
         <div className={"flex flex-col min-h-screen"}>
             <Navbar />
-            {children}
+          <div className="flex-1 bg-[#f4f4f0]">
+              {children}
+          </div>
+            <Footer />
         </div>
     )
 }
